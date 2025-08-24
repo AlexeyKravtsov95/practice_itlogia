1d929d3a67d816d849c5ed760dac9af7cbb50de0
AlexeyKravtsov95 <alexey.kravtsov95@gmail.com>
2025-08-24 18:00:31 +0700

    Added practice

--- itlogia-js-library-practice/scripts/script.js
+++ itlogia-js-library-practice/scripts/script.js
@@ -1,3 +1,16 @@
+new WOW({
+    animateClass: "animate__animated",
+}).init();
+
+$('.open-popup-link').magnificPopup({
+    type:'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+});
+
+$('.product-image').magnificPopup({
+    type:'image',
+});
+
 $('h1').html("Самая крутая пицца ждет <span>только в нашем ресторане</span>");
 $('#products-title').css('color', '#000000');
 
@@ -29,10 +42,11 @@ $('.btn-add-to-cart').click((e) => {
     $("order")[0].scrollIntoView({behavior: "smooth"})
 });
 
+let phoneInput = $("#phone-input");
+phoneInput.inputmask({"mask": "(999) 999-9999"});
 
 $("#create-order").click(function () {
     let addressInput = $("#address-input");
-    let phoneInput = $("#phone-input");
 
     if (!productInput.val()) {
         alert("Заполните пиццу");
